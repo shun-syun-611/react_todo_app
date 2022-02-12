d3c1dd1ba7c94c326cda67df9773365decdd06bf
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2022-02-12 19:10:11 +0900

    add: フォームの静的な部分をstateで動的に値を入力、操作できるように対応

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
  
 export const App  = () => {
+
+  // stateの設定
+  const [inCompleteTodos, setInCompleteTodos] = useState(["初期値", "変更値"]);
+  const [todoText, setTodoText] = useState('');
+  const [active, setActive] = useState(false);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const classToggle = (index) => setActive(!active);
+
+  // ボタンクリック処理
+  const onClickAdd = () => {
+    if(todoText === '') return;
+      const addTodos = [...inCompleteTodos, todoText];
+      setInCompleteTodos(addTodos);
+      setTodoText('');
+  }
+  const onClickDelete = (index) => {
+    const newTodos = [...inCompleteTodos];
+    newTodos.splice(index, 1);
+    setInCompleteTodos(newTodos);
+  }
+  // const onClickDone = (index) => {
+  //   classToggle(index);
+  // }
+
   return (
     <>
     <div className="js_todo_app">
     <h1>JS ToDoアプリ</h1>
     <form>
-        <input id="task_value" type="text" />
+        <input id="task_value" type="text" value={todoText} onChange={onChangeTodoText}/>
         <label for="">期限日:
             <select id="select_year" name="year">
               <option selected="true">2022</option>
@@ -22,20 +47,24 @@ export const App  = () => {
               <option>13</option>
             </select>
         </label>
-        <button id="task_submit" type="button">Add task</button>
+        <button id="task_submit" type="button" onClick={onClickAdd}>Add task</button>
     </form>
 
     <div className="todo_task_table">
         <h2>todo_task</h2>
         <ul id="task_list">
-          {/* ここはjsで動的に出力する */}
-        <li>
-          <p className="js_done_text">タスク内容</p>
-            <button className="js_delete_btn">Delete</button>
-            <button className="js_done_btn">Done</button>
-          <p className="js_deadline">期限日: 2022年2月11日</p>
-          </li>
-          {/* ここはjsで動的に出力する ここまで*/}
+          {/* liはjsで動的に出力する */}
+          {inCompleteTodos.map((todo, index) => {
+            return (
+              <li key={todo}>
+                <p className={active ? 'js_done_text' : ''}>{todo}</p>
+                <button className="js_delete_btn" onClick={() =>onClickDelete(index)}>Delete</button>
+                <button className="js_done_btn" onClick={() => classToggle(index)}>Done</button>
+                <p className="js_deadline">期限日: 2022年2月11日</p>
+              </li>
+            );
+          })}
+          {/* liはjsで動的に出力する ここまで*/}
         </ul>
     </div>
 </div>
